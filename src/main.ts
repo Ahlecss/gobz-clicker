dba77145239aa3f0a9acf33dfc4619caa4570844
Ahlecss <alex.sejourne1@gmail.com>
2024-11-21 19:39:44 +0100

    feat(state)

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,12 @@ class App {
 
   init() {
     const counter = Counter.getInstance();
-    const arnaud = new Teacher(1, 'Arnaud')
-    const christian = new Teacher(100, 'Christian')
-    const vincent = new Teacher(1000, 'Vincent')
-    const Mathieu = new Teacher(10000, 'Mathieu')
-    const Xavier = new Teacher(100000, 'Xavier')
-    const Corinne = new Teacher(1000000, 'Corine')
+    const arnaud = new Teacher(1, 'Arnaud', 1)
+    const christian = new Teacher(100, 'Christian', 3)
+    const vincent = new Teacher(1000, 'Vincent', 10)
+    const Mathieu = new Teacher(10000, 'Mathieu', 30)
+    const Xavier = new Teacher(100000, 'Xavier', 100)
+    const Corinne = new Teacher(1000000, 'Corine', 10000)
   }
 
 
--- src/teacher.ts
+++ src/teacher.ts
@@ -3,19 +3,25 @@ import { Counter } from "./counter";
 export class Teacher {
     count: number;
     price: number;
+    generate: number;
     name: string;
+    liTeacher: HTMLElement;
     elName: HTMLElement;
+    elGenerate: HTMLElement;
     elPrice: HTMLElement;
     elCount: HTMLElement;
     counter: Counter;
 
-    constructor(price: number, name: string) {
+    constructor(price: number, name: string, generate: number) {
         this.count = 0;
         this.price = price;
         this.name = name;
+        this.generate = generate;
+        this.liTeacher = document.createElement('li')
         this.elName = document.createElement('h2')
         this.elPrice = document.createElement('h3')
-        this.elCount = document.createElement('h4')
+        this.elGenerate = document.createElement('h4')
+        this.elCount = document.createElement('h5')
         this.init()
         this.addThunes()
 
@@ -23,37 +29,45 @@ export class Teacher {
 
     }
     init() {
-        console.log('ici')
         const teacherList = document.getElementById('teachers')!
-        const liTeacher = document.createElement('li')
         // const elBlock = document.createElement('div')
         // const teacherImage = document.createElement('img')
         this.elName.textContent = this.name
         this.elPrice.textContent = `Prix : ${this.price}`
-        this.elCount.textContent = `Nombre : ${this.count}`
+        this.elCount.textContent = `Quantite : ${this.count}`
+        this.elGenerate.textContent = `Génére : ${this.generate}`
 
-        liTeacher.appendChild(this.elName)
-        liTeacher.appendChild(this.elPrice)
-        liTeacher.appendChild(this.elCount)
-        teacherList.appendChild(liTeacher)
+        this.liTeacher.appendChild(this.elName)
+        this.liTeacher.appendChild(this.elPrice)
+        this.liTeacher.appendChild(this.elGenerate)
+        this.liTeacher.appendChild(this.elCount)
+        teacherList.appendChild(this.liTeacher)
         // Ajouter un item dans le template
         // Lance un set interval
-        liTeacher.addEventListener('click', this.buyTeacher.bind(this))
+        this.liTeacher.addEventListener('click', this.buyTeacher.bind(this))
     }
 
     buyTeacher() {
-        if(this.counter.gobzPoints < this.price) return
+        if (this.counter.gobzPoints < this.price) return
         this.counter.subPoints(this.price)
         this.count += 1;
         this.price += 1;
         console.log(this.count)
         this.elPrice.textContent = `Prix : ${this.price}`
-        this.elCount.textContent = `Nombre : ${this.count}`
+        this.elCount.textContent = `Quantité : ${this.count}`
+        this.elGenerate.textContent = `Génére : ${this.generate}`
+    }
+
+    checkPoints(): void {
+        if (this.counter.gobzPoints >= this.price && !this.liTeacher.classList.contains("teacherClickable")) {
+            this.liTeacher.classList.add("teacherClickable");
+        }
     }
 
     addThunes() {
         const interval = setInterval(() => {
             this.counter.addPoints(this.count)
+            this.checkPoints()
         }, 1000);
     }
 }
